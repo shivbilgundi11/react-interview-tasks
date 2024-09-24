2d15199737ddb8fbacef67ecdc86274955f6cbd3
Shivaprasad Bilgundi <shivbbilgundi11@gmail.com>
2024-09-24 18:05:08 +0530

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col items-center justify-center gap-y-5">
+        <h1 className="text-3xl font-bold underline">Hello world...!</h1>
+      </div>
+    </>
+  );
+}
